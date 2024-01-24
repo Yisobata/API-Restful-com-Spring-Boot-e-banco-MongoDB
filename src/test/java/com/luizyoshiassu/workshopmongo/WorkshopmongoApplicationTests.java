e068bb67fed51389f41a2add55fa39ad6230d9eb
Yisobata <luiz.isobata@gmail.com>
2024-01-24 03:47:28 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/luizyoshiassu/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizyoshiassu.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
