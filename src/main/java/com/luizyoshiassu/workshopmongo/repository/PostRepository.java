3c34444cd920ebebed428b1db0202c70a5066fcf
Yisobata <luiz.isobata@gmail.com>
2024-01-24 13:00:06 -0300

    Consulta simples com query methods

--- src/main/java/com/luizyoshiassu/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luizyoshiassu/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.luizyoshiassu.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.luizyoshiassu.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
